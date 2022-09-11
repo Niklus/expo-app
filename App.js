9ffe4b4d1a5d4d423767f84ed2b4da4f9b47bb1c
Nicholas Otieno <niklasoti@gmail.com>
2022-09-11 16:50:05 +0200

    Update

--- App.js
+++ App.js
@@ -1,32 +1,68 @@
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import * as ImageManipulator from "expo-image-manipulator";
+
+import logo from "./assets/logo.png";
+import styles from "./styles";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [selectedImage, setSelectedImage] = React.useState(null);
 
-  const increment = () => {
-    setCount(count + 1);
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    setSelectedImage({ localUri: pickerResult.uri });
   };
+
+  let openShareDialogAsync = async () => {
+    if (Platform.OS === "web") {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+
+    const imageTmp = await ImageManipulator.manipulateAsync(
+      selectedImage.localUri
+    );
+
+    await Sharing.shareAsync(imageTmp.uri);
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={styles.largeText}>{count}</Text>
-      <Button onPress={increment} title="increment" />
+      <Image source={logo} style={styles.logo} />
+
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
+
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  largeText: {
-    fontSize: 62,
-    fontWeight: "bold",
-  },
-});
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# Expo App
+# Expo App - Image Share
 
 Learning React Native with [Expo](https://expo.dev/) - a framework to build React Native apps.
